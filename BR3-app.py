5d2793c94c5d7d1256d6e8a55ed03ad7139ce6cb
Ilarion Zubialevich <ilarionzubialevich@gmail.com>
2025-02-12 23:24:10 +0000

    work for 12/02/25
    still have to finish create stage

--- BR3-app.py
+++ BR3-app.py
@@ -1,11 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "hello"
-@app.route("/name")
-def name():
-    return "lar"
-    
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=6006, debug=False)
+import pandas as pd
+
+df = pd.read_csv("cleanData.csv")
+print(df["startYear"].max())
